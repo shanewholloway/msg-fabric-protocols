d1ee2ae1a8ea2be13cdfd9a120fded19db63b5c5
Shane Holloway <shane.holloway@ieee.org>
2018-02-15 13:55:58 -0700

    Updated rollup config and removed browser specifics

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,14 @@
-import rpi_babel from 'rollup-plugin-babel'
-import rpi_replace from 'rollup-plugin-replace'
+import pkg from './package.json'
+import rpi_jsy from 'rollup-plugin-jsy-babel'
 
 const sourcemap = 'inline'
-
 const external = ['crypto']
+const plugins = [rpi_jsy()]
 
-const plugins = [jsy_plugin()]
-const replace_prod = {'process.env.NODE_ENV': "'production'"}
-const plugins_prod = plugins.concat([rpi_replace({values: replace_prod})])
-
-export default [
-	{ input: 'code/index.jsy',
-		output: [
-      { file: `dist/index.js`, format: 'cjs' },
-      { file: `dist/index.mjs`, format: 'es' },
-    ],
-    sourcemap, external, plugins },
-
-	{ input: 'code/index.nodejs.jsy',
-		output: [
-      { file: `dist/nodejs.js`, format: 'cjs' },
-      { file: `dist/nodejs.mjs`, format: 'es' },
-    ],
-    sourcemap, external, plugins: plugins_prod },
-
-	{ input: 'code/index.browser.jsy',
-		output: [
-      { file: `dist/browser.js`, format: 'cjs' },
-      { file: `dist/browser.mjs`, format: 'es' },
-      { file: `dist/browser.amd.js`, format: 'amd' },
-    ],
-    sourcemap, external:[], plugins: plugins_prod },
-
-]
-
-
-
-
-function jsy_plugin() {
-  const jsy_preset = [ 'jsy/lean', { no_stage_3: true, modules: false } ]
-  return rpi_babel({
-    exclude: 'node_modules/**',
-    presets: [ jsy_preset ],
-    plugins: [],
-    babelrc: false }) }
+export default {
+	input: 'code/index.jsy',
+  output: [
+    { file: pkg.main, format: 'cjs', sourcemap, exports:'named' },
+    { file: pkg.module, format: 'es', sourcemap },
+  ],
+  external, plugins }
